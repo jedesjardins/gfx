681039548188ea823aa47296bd38a5dfbb30336b
jedesjardins <jedesjardins@umass.edu>
2019-09-27 11:56:35 -0400

    Update to drawing with multiple bound buffers

--- examples/mul_vbs.cpp
+++ examples/mul_vbs.cpp
@@ -27,7 +27,10 @@
 #include <utility>
 #include <algorithm>
 
-std::vector<glm::vec2> pos_vec{{0.f, -.5f}, {.5f, .5f}, {-.5f, .5f}};
+std::vector<glm::vec2> left_pos_vec{{-1.f, -1.f}, {0.f, 0.f}, {-1.f, 1.f}};
+
+std::vector<glm::vec2> right_pos_vec{{1.f, -1.f}, {0.f, 0.f}, {1.f, 1.f}};
+
 std::vector<glm::vec3> color_vec{{1.f, 0.f, 0.f}, {0.f, 1.f, 0.f}, {0.f, 0.f, 1.f}};
 
 std::vector<uint32_t> indices{0, 1, 2};
@@ -63,14 +66,15 @@ int main()
 
     auto pipeline = render_device.get_pipeline_handle("mul_vbs_shader").value();
 
+    // Both vertex bindings in their own buffer
     auto pos_buffer = render_device
                           .create_buffer(
-                              pos_vec.size() * sizeof(glm::vec2),
+                              left_pos_vec.size() * sizeof(glm::vec2),
                               VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
                               VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT)
                           .value();
 
-    render_device.update_buffer(pos_buffer, pos_vec.size() * sizeof(glm::vec2), pos_vec.data());
+    render_device.update_buffer(pos_buffer, left_pos_vec.size() * sizeof(glm::vec2), left_pos_vec.data());
 
     auto color_buffer = render_device
                             .create_buffer(color_vec.size() * sizeof(glm::vec3),
@@ -81,6 +85,25 @@ int main()
 
     render_device.update_buffer(
         color_buffer, color_vec.size() * sizeof(glm::vec3), color_vec.data());
+
+
+    // Both vertex bindings in one buffer
+    std::array<float, 3 * 2 + 3 * 3> vertices;
+
+    float * vec_1 = vertices.data();
+    float * vec_2 = vec_1 + (right_pos_vec.size() * 2);
+
+    memcpy(vec_1, right_pos_vec.data(), right_pos_vec.size() * sizeof(glm::vec2));
+    memcpy(vec_2, color_vec.data(), color_vec.size() * sizeof(glm::vec3));
+
+    auto vertex_buffer = render_device
+                             .create_buffer(vertices.size() * sizeof(float),
+                                            VK_BUFFER_USAGE_TRANSFER_DST_BIT
+                                                | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+                                            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT)
+                             .value();
+
+    render_device.update_buffer(vertex_buffer, vertices.size() * sizeof(float), vertices.data());
 
     auto index_buffer = render_device
                             .create_buffer(
@@ -96,22 +119,42 @@ int main()
     {
         glfwPollEvents();
 
-        gfx::DrawParameters params{};
+        // draw with different buffers
+        gfx::DrawParameters params_1{};
 
-        std::array<gfx::BufferHandle, 2> vertex_buffers{pos_buffer, color_buffer};
-        std::array<VkDeviceSize, 2>      vertex_buffer_offsets{0, 0};
+        std::array<gfx::BufferHandle, 2> vertex_buffers_1{pos_buffer, color_buffer};
+        std::array<VkDeviceSize, 2>      vertex_buffer_offsets_1{0, 0};
 
-        params.pipeline = pipeline;
+        params_1.pipeline = pipeline;
 
-        params.vertex_buffer_count   = 2;
-        params.vertex_buffers        = vertex_buffers.data();
-        params.vertex_buffer_offsets = vertex_buffer_offsets.data();
+        params_1.vertex_buffer_count   = 2;
+        params_1.vertex_buffers        = vertex_buffers_1.data();
+        params_1.vertex_buffer_offsets = vertex_buffer_offsets_1.data();
 
-        params.index_buffer        = index_buffer;
-        params.index_buffer_offset = 0;
-        params.index_count         = 3;
+        params_1.index_buffer        = index_buffer;
+        params_1.index_buffer_offset = 0;
+        params_1.index_count         = 3;
 
-        render_device.draw(params);
+        render_device.draw(params_1);
+
+
+        // draw with the same buffer twice
+        gfx::DrawParameters params_2{};
+
+        std::array<gfx::BufferHandle, 2> vertex_buffers_2{vertex_buffer, vertex_buffer};
+        std::array<VkDeviceSize, 2>      vertex_buffer_offsets_2{0, right_pos_vec.size() * sizeof(glm::vec2)};
+
+        params_2.pipeline = pipeline;
+
+        params_2.vertex_buffer_count   = 2;
+        params_2.vertex_buffers        = vertex_buffers_2.data();
+        params_2.vertex_buffer_offsets = vertex_buffer_offsets_2.data();
+
+        params_2.index_buffer        = index_buffer;
+        params_2.index_buffer_offset = 0;
+        params_2.index_count         = 3;
+
+        render_device.draw(params_2);
 
         draw_success = render_device.submit_frame();
     }
